d5fbd2d095fbbf1416082f8103b572874c11b4e9
zackjacksongit <z.jackson58@gmail.com>
2018-09-16 21:37:20 -0400

    Login required middleware

--- middleware/index.js
+++ middleware/index.js
@@ -5,4 +5,15 @@ function loggedOut(req, res, next) {
   return next();
 }
 
+function requiresLogin(req, res, next) {
+  if (req.session && req.session.userId) {
+    return next();
+  } else {
+    var err = new Error("Please log in to view this page.");
+    err.status = 401;
+    return next(err);
+  }
+}
+
 module.exports.loggedOut = loggedOut;
+module.exports.requiresLogin = requiresLogin;
