f817516dcd4362c871c9848de6aec22c74eda412
zackjacksongit <z.jackson58@gmail.com>
2018-09-16 20:31:56 -0400

    Auth username and password

--- models/user.js
+++ models/user.js
@@ -24,6 +24,26 @@ var UserSchema = new mongoose.Schema({
   }
 });
 
+// Authenticate input against database documents
+UserSchema.statics.authenticate = function(email, password, callback) {
+  User.findOne({ email: email }).exec(function(error, user) {
+    if (error) {
+      return callback(error);
+    } else if (!user) {
+      var err = new Error("User not found.");
+      err.status = 401;
+      return callback(err);
+    }
+    bcrypt.compare(password, user.password, function(error, result) {
+      if (result === true) {
+        return callback(null, user);
+      } else {
+        return callback();
+      }
+    });
+  });
+};
+
 // Hash password before saving to database
 UserSchema.pre("save", function(next) {
   var user = this;
